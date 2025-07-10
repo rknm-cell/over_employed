ac1542a03eaa4ca913fec5973fb81da97a9b9650
rknm-cell <racknahm@gmail.com>
2025-07-10 10:56:44 -0400

    updated printer assets

--- over_employed/player.gd
+++ over_employed/player.gd
@@ -10,7 +10,6 @@ func _ready():
 	screen_size = get_viewport_rect().size
 
 func _physics_process(delta):
-	var velocity = Vector2.ZERO
 
 	if Input.is_action_pressed("move_right"):
 		velocity.x += 1
